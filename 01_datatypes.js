7214d79699ad2ccdc40a4acd502f165754b7468e
ashraf840 <shift2unleashed2011@gmail.com>
2023-03-08 19:36:20 +0600

    Initial commit: datatypes

---
+++ 01_datatypes.js
@@ -0,0 +1,11 @@
+/*  AGENDA
+- Data Types  [ string, number, boolean, array, enum, tuple, any, void, never ]
+- Type Inefrence
+- Union & Intersection Types
+- Generics
+- Literal Types
+*/
+var lname;
+lname = 'John';
+var newName = lname.toUpperCase();
+console.log(newName);
