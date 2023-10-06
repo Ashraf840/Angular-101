3bca251cf55e51c2a77ad63f86019fff48b55026
Ashraf840 <shift2unleashed2011@gmail.com>
2023-10-06 13:45:56 +0600

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Crash Course of TypeScript
+Vid Ref: https://www.youtube.com/watch?v=30LWjhZzg50&t=6603s
+
+Github Repo: https://github.com/Ashraf840/Angular-101
+Branch (New): typescript_crash_course_101
---
+++ src/ts_intro_1.ts
@@ -0,0 +1,7 @@
+console.log("Hellow TypeScript!");
+
+/*
+ * Since we're using typescript without a framework, to transpile a typescript file into a specific directory, use the following command:
+ * [Skeleton]     tsc file.ts --outDir /PATH/
+ * [Original]     tsc src/ts_intro_1.ts --outDir ../bin/
+ */
