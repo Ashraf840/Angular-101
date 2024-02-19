793a472964689cd3c299990efcaa8f6ea034d0b9
Ashraf840 <shift2unleashed2011@gmail.com>
2024-02-19 11:19:08 +0600

    Datatypes: enums, reducing LOCs of enums while compile down to js, tuples

--- TypescriptDemo/datatypes.js
+++ TypescriptDemo/datatypes.js
@@ -24,7 +24,35 @@ console.log("names:", names);
 var generics_number; // Generics; Define as an array type of "Number"; Generics
 generics_number = [1, 2, 3, 4, 5];
 console.log("generics_number:", generics_number);
+// JS Function: Filter
 var numlist_gt_2 = generics_number.filter(function (num) { return num > 2; });
 console.log("Numbers greater than 2:", numlist_gt_2);
-var name_find_result = names.find(function (name) { return name === "Tanjim3"; });
-console.log("Result:", name_find_result);
+// JS Function: Find (not working, so removed)
+// JS Function: Reduce
+var aggregated_generic_numbers = generics_number.reduce(function (acc, cnum) { return acc + cnum; });
+console.log("Aggregated num result:", aggregated_generic_numbers);
+// Enums (Enumerator)
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+console.log("Enums Color:", Color); // Output: { '0': 'Red', '1': 'Green', '2': 'Blue', Red: 0, Green: 1, Blue: 2 }
+// Define a set of color code; Usecase: Senoir frontend dev defined a set of colors that will be used accross the system development
+var ColorSet;
+(function (ColorSet) {
+    ColorSet["Red"] = "#d62246";
+    ColorSet["Green"] = "#0e7c7b";
+    ColorSet["Blue"] = "#88b7b5";
+})(ColorSet || (ColorSet = {}));
+console.log("Color set:", ColorSet); // Output: { Red: '#d62246', Green: '#0e7c7b', Blue: '#88b7b5' }
+console.log("Color set-2:", "#d62246" /* ColorSet2.Red */); // Output: #d62246
+// Datatypes: Tuple
+// Fixed number of array elements are defined while instantiation
+var swapNums;
+function swapNumber(num1, num2) {
+    return [num2, num1];
+}
+swapNums = swapNumber(10, 20);
+console.log("Swapped nums:", swapNums);
--- TypescriptDemo/datatypes.ts
+++ TypescriptDemo/datatypes.ts
@@ -29,12 +29,47 @@ let generics_number: Array<number>; // Generics; Define as an array type of "Num
 generics_number = [1, 2, 3, 4, 5];
 console.log("generics_number:", generics_number);
 
+// JS Function: Filter
 let numlist_gt_2 = generics_number.filter((num) => num > 2);
 console.log("Numbers greater than 2:", numlist_gt_2);
 
+// JS Function: Find (not working, so removed)
 
+// JS Function: Reduce
+let aggregated_generic_numbers = generics_number.reduce((acc, cnum) => acc + cnum);
+console.log("Aggregated num result:", aggregated_generic_numbers);
 
+// Enums (Enumerator)
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+console.log("Enums Color:", Color); // Output: { '0': 'Red', '1': 'Green', '2': 'Blue', Red: 0, Green: 1, Blue: 2 }
 
+// Define a set of color code; Usecase: Senoir frontend dev defined a set of colors that will be used accross the system development
+enum ColorSet {
+    Red = "#d62246",
+    Green = "#0e7c7b",
+    Blue = "#88b7b5"
+}
+console.log("Color set:", ColorSet); // Output: { Red: '#d62246', Green: '#0e7c7b', Blue: '#88b7b5' }
 
+// Decrease the LOCs while compiled down to js, trimmed down some bytes from production package
+const enum ColorSet2 {
+    Red = "#d62246",
+    Green = "#0e7c7b",
+    Blue = "#88b7b5"
+}
+console.log("Color set-2:", ColorSet2.Red); // Output: #d62246
 
+// Datatypes: Tuple
+// Fixed number of array elements are defined while instantiation
+let swapNums: [number, number];
 
+function swapNumber(num1: number, num2: number): [number, number] {
+    return [num2, num1]
+}
+
+swapNums = swapNumber(10, 20);
+console.log("Swapped nums:", swapNums);
