89810135ae532beff0eb9dcd03f7b4eca31f71b7
Ashraf840 <shift2unleashed2011@gmail.com>
2024-02-27 22:35:56 +0600

    Lifecycle Hook: ngAfterContentInit; to  to access the contents used @ContentChild() decorator

--- AngularDemo/hotelinventoryapp/src/app/app.module.ts
+++ AngularDemo/hotelinventoryapp/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
 import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { DummyContentComponent } from './dummy-content/dummy-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoomsComponent,
     RoomsListComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContainerComponent,
+    EmployeeComponent,
+    DummyContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularDemo/hotelinventoryapp/src/app/container/container.component.ts
@@ -0,0 +1,27 @@
+import { AfterContentInit, Component, ContentChild, OnInit } from '@angular/core';
+import { EmployeeComponent } from '../employee/employee.component';
+
+@Component({
+  selector: 'hinv-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent implements AfterContentInit, OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  // Originally loaded in the app component, after loading the content, thay can be accessed in this content projection. Thus can be accessed within the "ngAfterContentInit" lifecycle. And by default the "static=false" for @ContentChild & cannot be changed.
+  // The contents are always routed from the parent component where they are actually loaded, thus it make sense to not having "static=true" in "@ContentChild" decorator.
+  @ContentChild(EmployeeComponent)
+  empc!: EmployeeComponent;
+
+  // Content which are loaded using "ng-content" should be access within this lifecycle hook
+  ngAfterContentInit(): void {
+    console.log("Employee component (ngAfterContentInit of Container Component):", this.empc);
+    // Modify a prop value of Employee Component after loading the content in parent component.
+    this.empc.empName += " (changed in ngAfterContentInit)"
+  }
+
+}
