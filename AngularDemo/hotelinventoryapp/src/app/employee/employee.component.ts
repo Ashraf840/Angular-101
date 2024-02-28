9c05f554dc22e5cc6cff79905e369a8a42ecd4e5
Ashraf840 <shift2unleashed2011@gmail.com>
2024-02-28 07:52:49 +0600

    Created a room service to move some busi business logic there; understand Dependency Injection; understand singleton design pattern while invoking the service through injecting them into the component constructor

--- AngularDemo/hotelinventoryapp/src/app/employee/employee.component.ts
+++ AngularDemo/hotelinventoryapp/src/app/employee/employee.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from '../rooms/services/room.service';
 
 @Component({
   selector: 'hinv-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
+  providers: [RoomService]
 })
 export class EmployeeComponent implements OnInit {
   empName: string = "John";
 
-  constructor() { }
+  // NB: Even if we inject roomService here, the service class will only be initialized once inside the 'room.service' file as the global instance by registering the service globally inside the "providers" section of the "app.nodule" file.
+  // If we want to separately initialize the roomService here, then we need to register the service into the "providers" section separately into this component as well.
+  constructor(private roomservice: RoomService) { }
 
   ngOnInit(): void { }
 }
